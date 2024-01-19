af1138e9c94dc0dbbe4d7ecac40279555e871b4e
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-19 01:53:21 -0800

    how to make a scrpt dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from sys import argv
+    import sys
 
-    number_args = len(argv) - 1
+    number_args = len(sys.argv) - 1
 
     if number_args == 0:
         print("0 arguments.")
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         print("{} {}:".format(number_args, 'argument' if number_args == 1
               else 'arguments'), end='\n' if number_args == 1 else '.\n')
 
-        for i, arg in enumerate(argv[1:], start=1):
+        for i, arg in enumerate(sys.argv[1:], start=1):
             print("{}: {}".format(i, arg))
