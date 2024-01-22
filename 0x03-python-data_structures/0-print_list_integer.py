f6e28cfb00eddff500bdb8a722bc8d095d9952d5
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-22 12:13:44 -0800

    print integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for number in my_list:
-        if type(number) == int:
-            print("{}".format(number))
+        print("{:d}".format(number))
