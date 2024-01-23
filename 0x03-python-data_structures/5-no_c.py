6b331829f442feea54bf175279ea281e530ba682
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-23 00:43:50 -0800

    removing c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
+    if not my_string:
+        return my_string
+
     new_string = ""
     for char in my_string:
         if char != 'c' and char != 'C':
