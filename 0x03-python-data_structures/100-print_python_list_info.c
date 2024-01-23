a4112cc1904ad98c0a64bf984371d0c1da2c2472
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-23 13:21:37 -0800

    python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <Python.h>
+#include <stdio.h>
+#include <object.h>
+#include <listobject.h>
+
+voiid print_python_list_info(PyObject *p)
+{
+	PyListObject *List = (PyListObject *)p;
+	Py_ssize_t size, i;
+	PyObject *element;
+
+	if (!PyList_Check(p))
+	{
+		fprintf(stderr, "Invalid argument: not a Python List.\n");
+		return;
+	}
+
+	size = PyList_Size(p);
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		element = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(element)->tp_name);
+	}
+}
