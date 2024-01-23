2d5b0ab0e5a99574994d74196d03035d8716db8e
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-23 00:30:16 -0800

    removing c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_string = ""
     for char in my_string:
-        if char.lower() not in ('c', 'C'):
+        if char != 'c' amd char != 'C':
             new_string += char
 
     return (new_string)
