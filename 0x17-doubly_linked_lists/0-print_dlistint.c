18417fb92a527bbf9d0d63e9ef12aa50674788fd
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-25 05:04:02 -0800

    prints all the elements of a dlistint_t list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -15,6 +15,6 @@ size_t print_dlistint(const dlistint_t *h)
 		printf("%d\n", current->n);
 		nodes_number++;
 	}
-	printf("\n");
+	printf("-> %zu elements\n", nodes_number);
 	return (nodes_number);
 }
