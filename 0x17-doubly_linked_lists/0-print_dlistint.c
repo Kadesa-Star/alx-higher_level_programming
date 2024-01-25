9155060d9e191c52febee75aadb6604f8f74ac73
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-25 05:03:03 -0800

    prints all the elements of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all elements of a DLL
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t nodes_number = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		nodes_number++;
+	}
+	printf("\n");
+	return (nodes_number);
+}
