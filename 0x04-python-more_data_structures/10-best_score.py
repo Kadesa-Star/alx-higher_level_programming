8a4967b6360650a08e98a04340090d007e8d7615
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-26 10:51:29 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
 
-    best max(a_dictionary, key=a_dictionary.get)
+    best = max(a_dictionary, key=a_dictionary.get)
 
     return (best)
