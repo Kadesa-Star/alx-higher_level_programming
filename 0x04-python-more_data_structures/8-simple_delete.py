3464aa3d5f50b4b4baa6603659fe894e0a527e81
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-26 10:31:58 -0800

    deletekey

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if not isinstance(key, str):
-        raise TypeError("Key must be a string")
-
     if key in a_dictionary:
         del a_dictionary[key]
+
+    return (a_dictionary)
