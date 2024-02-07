1b68c1bcb398e803c1523270ee4a0e89c77024f6
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-02-07 02:47:01 -0800

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """a class square that defines  as square based on 0-square.py"""
+
+    def __init__(self, size):
+        """this initializes a square with given size.
+
+        Args:
+            size (int): size of the square
+        """
+
+        self.__size = size
