c3be3d8cc64f8034996ea127f5e027f9aea707a5
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-02-20 14:10:07 -0800

    load, add and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """ script that adds all arguments to a Python list"""
-
-
 import sys
+
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-# get args excluding script name
-arguments = sys.argv[1:]
 
 # load existing items fromfile if it exists
 try:
     existing_items = load_from_json_file("add_item.json")
 except FileNotFoundError:
     existing_items = []
+
+arguments = sys.argv[1:]
 
 # Add new items to the existing list
 existing_items.extend(arguments)
