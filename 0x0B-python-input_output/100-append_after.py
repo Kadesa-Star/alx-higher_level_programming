786e481f25afd7d3b25d80e414342bfb928135f0
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-02-21 04:55:59 -0800

    search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  inserts a line of text to a file, after each line 
+"""  inserts a line of text to a file, after each line
 containing a specific string """
 
 
@@ -14,16 +14,11 @@ def append_after(filename="", search_string="", new_string=""):
         new_string (str): line of text to insert after each
         text containing the search string
     """
-    # open the file in read mode
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-
-    # open the file in write mod
-    with open(filename, 'w') as file:
-        #iterate over the lines
-        for line in lines:
-            #write the orginal line to the file
-            file.write(line)
-            #if search string is found append new string
+    lines = ""
+    with open(filename) as file:
+        for line in file:
+            lines += line
             if search_string in line:
-                file.write(new_string + '\n')
+                line += new_string
+    with open(filename, 'w') as w:
+        w.write(lines)
