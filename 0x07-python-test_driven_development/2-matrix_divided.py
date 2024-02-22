845a903c1dd46359e9281669e905f822ffcd2e15
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-02-22 04:37:48 -0800

    divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,26 +18,27 @@ def matrix_divided(matrix, div):
         ZeroDivisionError: if div is equal to 0
         TypeError: if each row doesnt have same size
     """
-    #validate matrix
+    # validate matrix
     if (not isinstance(matrix, list) or matrix == [] or
         not all(isinstance(row, list) for row in matrix) or
-        not all ((isinstance(element, int) or isinstance(element, float))
-            for element in [num for row in matrix for num in row])):
+        not all((isinstance(element, int) or isinstance(element, float))
+                for element in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists)"
                         "of integers/floats")
 
-    #check if each row of the matrix has the same size
+    # check if each row of the matrix has the same size
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    #validate div
+    # validate div
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    #divide each matrix element by div and round to 2 dp
-    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    # divide each matrix element by div and round to 2 dp
+    new_matrix = [[round(element / div, 2)
+                   for element in row] for row in matrix]
 
     return new_matrix
