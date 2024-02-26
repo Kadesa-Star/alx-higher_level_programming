f7a791a0e1dd63e18226a8f2087098ce2094df71
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-02-26 14:35:22 -0800

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Represent a rectangle."""
+    """Represent a rectangle class with height and width, x , y and id."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
