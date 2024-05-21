4e1b02cfb1c9ee423e53c45d8bd4321266096b8e
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-21 23:47:14 +0300

    displays the average temperature

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
 -- Import the table dump into the hbtn_0c_0 database
 -- Display the average temperature (Fahrenheit) by city ordered by temperature (descending)
-SELECT `city`, AVG(`Temperature`) AS `average_Temperature_Fahrenheit`
+SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
 GROUP BY `city`
-ORDER BY `average_Temperature_Fahrenheit` DESC;
+ORDER BY `avg_temp` DESC;
 
