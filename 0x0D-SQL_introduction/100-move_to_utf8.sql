5f30aa2e146229dd0a59ec1c039916c439b02a6a
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-21 23:37:08 +0300

    hbtn_0c_0 database to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,6 @@
--- Convert the entire database hbtn_0c_0 to UTF8
+-- Convert the entire database hbtn_0c_0 to UTF8.
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the first_table table to UTF-8
+ALTER TABLE hbtn_0c_0.first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
