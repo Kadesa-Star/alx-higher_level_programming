839edae24da7a8d239e110e3cdcb817d08d16756
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-23 21:16:16 +0300

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
--- Creates the MySQL server user user_0d_1 with all privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Check if the user 'user_0d_1'@'localhost' exists and create if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
+-- Grant all privileges to 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply the changes
 FLUSH PRIVILEGES;
-
