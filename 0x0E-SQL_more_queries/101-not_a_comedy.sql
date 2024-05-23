fda46d1d8ac26e831f45d9a2d0c0afdb988f1a45
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-23 22:22:08 +0300

    lists all shows without the genre comedy

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -9,4 +9,5 @@ SELECT s.title
 FROM tv_shows s
 LEFT JOIN tv_show_genres sg ON s.id = sg.show_id AND sg.genre_id = @comedy_id
 WHERE sg.show_id IS NULL
+  AND (@comedy_id IS NULL OR s.id NOT IN (SELECT show_id FROM tv_show_genres WHERE genre_id = @comedy_id))
 ORDER BY s.title ASC;
