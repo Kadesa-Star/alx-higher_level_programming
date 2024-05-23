38e42bad60d0178391306f835582f672e4eb3911
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-23 22:50:02 +0300

    list all genres not linked to dexter

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,15 +1,11 @@
--- Lists all genres not linked to the show Dexter from hbtn_0d_tvshows
-USE hbtn_0d_tvshows;
-
--- First, find the id of the show "Dexter"
-SELECT id INTO @dexter_id FROM tv_shows WHERE title = 'Dexter';
-
--- List all genres that are not linked to "Dexter"
-SELECT name 
-FROM tv_genres 
-WHERE id NOT IN (
-    SELECT genre_id 
-    FROM tv_show_genres 
-    WHERE show_id = @dexter_id
-)
-ORDER BY name;
+-- Lists all genres not linked to the show Dexter
+SELECT DISTINCT tg.name
+FROM tv_genres tg
+LEFT JOIN (
+    SELECT ts.title, tsg.genre_id
+    FROM tv_shows ts
+    JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+    WHERE ts.title = 'Dexter'
+) AS dexter_genres ON tg.id = dexter_genres.genre_id
+WHERE dexter_genres.title IS NULL
+ORDER BY tg.name ASC;
