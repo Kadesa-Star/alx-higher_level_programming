c1cc9d57932e420233652d08eed28b20c19bfecc
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-23 23:01:40 +0300

    list hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,9 @@
 -- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 -- Records are ordered by descending rating.
-SELECT g.name AS genre, SUM(r.rate) AS rating
+SELECT g.name AS genre, COUNT(DISTINCT sg.show_id) AS num_shows, SUM(r.rate) AS total_rating
 FROM tv_genres AS g
-INNER JOIN tv_show_genres AS sg ON g.id = sg.genre_id
-INNER JOIN tv_show_ratings AS r ON sg.show_id = r.show_id
+LEFT JOIN tv_show_genres AS sg ON g.id = sg.genre_id
+LEFT JOIN tv_show_ratings AS r ON sg.show_id = r.show_id
 GROUP BY g.name
-ORDER BY rating DESC;
+HAVING total_rating IS NOT NULL
+ORDER BY total_rating DESC;
