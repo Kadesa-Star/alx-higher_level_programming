7a9d4bbc2bc9aa683112d8686c13d8da06035aab
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-23 22:42:54 +0300

    lists shows without the genre comedy

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,12 +1,13 @@
 -- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 USE hbtn_0d_tvshows;
 
-SELECT title
-FROM tv_shows
-WHERE id NOT IN (
+SELECT DISTINCT ts.title
+FROM tv_shows ts
+LEFT JOIN (
     SELECT sg.show_id
     FROM tv_show_genres sg
     JOIN tv_genres g ON sg.genre_id = g.id
     WHERE g.name = 'Comedy'
-)
-ORDER BY title ASC;
+) AS comedy_shows ON ts.id = comedy_shows.show_id
+WHERE comedy_shows.show_id IS NULL
+ORDER BY ts.title ASC;
