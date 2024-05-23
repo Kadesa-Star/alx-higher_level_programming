cbf450db7c4fc5ef12bd61ae9d96a539933b74ac
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-23 22:53:28 +0300

    lists shows without genre comedy

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,16 +1,21 @@
--- Lists all shows without the genre Comedy from hbtn_0d_tvshows
-USE hbtn_0d_tvshows;
+-- Lists all shows without the genre "Comedy" in the database hbtn_0d_tvshows.
+-- Records are ordered by ascending show title.
+SELECT DISTINCT t.title
+FROM tv_shows t
+LEFT JOIN tv_show_genres s ON s.show_id = t.id
+LEFT JOIN tv_genres g ON g.id = s.genre_id
+-- The query selects distinct show titles from the tv_shows table.
 
--- Select all shows that are not linked to the "Comedy" genre
-SELECT title
-FROM tv_shows
-WHERE id NOT IN (
-    SELECT show_id
-    FROM tv_show_genres
-    WHERE genre_id = (
-        SELECT id
-        FROM tv_genres
-        WHERE name = 'Comedy'
-    )
+WHERE t.title NOT IN (
+    -- Subquery: selects titles of shows with the genre "Comedy".
+    SELECT t.title
+    FROM tv_shows t
+    INNER JOIN tv_show_genres s ON s.show_id = t.id
+    INNER JOIN tv_genres g ON g.id = s.genre_id
+    WHERE g.name = 'Comedy'
 )
-ORDER BY title ASC;
+-- Filters out shows that have the genre "Comedy" by excluding their titles 
+-- using the NOT IN clause with the subquery.
+
+ORDER BY t.title;
+-- Orders the resulting shows by their title in ascending order.
