de77b99fd27b0992d22098bba6bd6ebdf91d28b7
Kadesa <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-22 12:52:02 +0300

    8-esrever.js
    Reversed list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 exports.esrever = function (list) {
   const reversList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    reversedList.push(list[i]);
+    reversedList.push (list[i]);
   }
   return reversedList;
 };
