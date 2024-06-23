1576b0fb7b27cb5c69816f85bd9a9b1387743cf7
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-23 23:27:37 +0300

    select all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,44 +2,35 @@
 import sys
 import MySQLdb
 
-
-def list_states(username, password, database):
-    # connect to MySQL server
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database,
-            charset='utf8'
-        )
-        with db.cursor() as cursor:
-            # Execute SQL query to fetch states sorted by id
-            cursor.execute("SELECT * FROM states ORDER BY id ASC")
-            # fetch all rows
-            states = cursor.fetchall()
-
-            # Display results as specified
-            for state in states:
-                print(state)
-    except MySQLdb.Error as e:
-        print(f"Error querying database: {e}")
-
-    finally:
-        # close cursor and connection
-        cursor.close()
-        db.close()
-
-
 if __name__ == "__main__":
     # check if the correct number of arguments are provided
     if len(sys.argv) != 4:
         sys.exit("Usage: ./0-select_states.py <username>"
                  "<password> <database>")
 
+    # Retrieve command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    # connect to MySQL server
+    connec = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset='utf8'
+    )
+    cursor = connec.cursor()
+    # Execute SQL query to fetch states sorted by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # fetch all rows
+    states = cursor.fetchall()
 
-    list_states(username, password, database)
+    # Display results as specified
+    for state in states:
+        print(state)
+
+    # close cursor and connection
+    cursor.close()
+    connec.close()
