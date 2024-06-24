f3063600ca91b055704152279f3f42b80148ea5f
Kadesa <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-24 13:38:50 +0300

    1-filter_states.py
    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script to fetch and display from the database sorted by states.id
+script to fetch and display states with names starting with 'N'
 """
 
 
@@ -8,35 +8,38 @@ import sys
 import MySQLdb
 
 
-if __name__ == "__main__":
-    # check if the correct number of arguments are provided
+if __name__ == '__main__':
+    # check if the correct number of arguments is provided
     if len(sys.argv) != 4:
-        sys.exit("Usage: ./0-select_states.py <username>"
-                 "<password> <database>")
+        sys.exit('Usage: ./1-filter_states.py <mysql username> <mysql password> <database name>'>)
 
     # Retrieve command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    # connect to MySQL server
-    connec = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset='utf8'
-    )
-    cursor = connec.cursor()
-    # Execute SQL query to fetch states sorted by id
-    cursor.execute("SELECT * FROM states")
-    # fetch all rows
-    states = cursor.fetchall()
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-    # Display results as specified
-    for state in states:
-        print(state)
+    # connect to MySQLdb server
+    conn = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name,
+            charset='utf8'
+            )
+    # create a cursor object using cursor() method
+    cursor = conn.cursor()
+
+    # execute SQL query to fetch states starting with 'N' sorted by id
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+    # Fetch all rows
+    query_rows = cursor.fetchall()
+
+    # Display results
+    for row in query_rows:
+        print(row)
 
     # close cursor and connection
     cursor.close()
-    connec.close()
+    conn.close()
