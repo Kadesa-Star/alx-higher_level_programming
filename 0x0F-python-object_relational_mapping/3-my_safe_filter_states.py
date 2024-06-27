5d1db2345554d6ba2499e48c5dfcbbba2d15e62c
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-27 18:08:31 +0300

    SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script to fetch and display states where name mataches the argument
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+  # Retrieve command line arguments
+  mysql_username = sys.argv[1]
+  mysql_password = sys.argv[2]
+  database_name = sys.argv[3]
+  state_name = sys.argv[4]
+
+
+  # connect to MySQL server
+  conn = MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user=mysql_username,
+    passwd=mysql_password,
+    db=databse_name,
+    )
+
+  # create a cursor object using cursor() method
+  cursor = conn.cursor()
+
+  # execute SQL query to fetch states where name matches the argument, sort by id
+  query = "SELECT * FROM states WHERE name = %s ORDER BY ID"
+  cursor.execute(query, (state_name))
+
+  # fetch all rows
+  query_rows = cursor.fetchall()
+
+  # display results
+  for row in query_rows:
+      print(row)
+
+  # close cursor and connection
+  cursor.close()
+  conn.close()
