28a88ee23dc78c6a88f40685ea3379fe9647358c
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-27 20:55:14 +0300

    first state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,38 @@
 #!/usr/bin/python3
 """
-Class definition of a State and an instance Base = declarative_base()
+Contains class definition of State and an instance Base = declarative_base()
 """
 
-import sys
-from model_state import Base, State
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import (create_engine)
+# Create an instance of the Base class
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class that links to the MySQL table 'states'
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    import sys
+    # Get MySQL credentials and database name from command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Create an engine that connects to the MySQL server
+    engine = create_engine(
+        f'mysql+mysqldb://{mysql_username}\
+                :{mysql_password}@localhost/{database_name}',
+        pool_pre_ping=True
+    )
+
+    # Create all tables defined by Base's subclasses (State in this case)
     Base.metadata.create_all(engine)
