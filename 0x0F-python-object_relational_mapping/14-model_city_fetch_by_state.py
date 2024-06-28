33da35fe332bf4a9766ada0fa7bef50dff8b3200
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-29 01:06:07 +0300

    cities in state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,31 @@
+# 14-model_city_fetch_by_state.py
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    # Database connection
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ), pool_pre_ping=True
+    )
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Query all City objects, sorted by id
+    cities = session.query(City).order_by(City.id).all()
+
+    # Print results in the required format
+    for city in cities:
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+
+    # Close session
+    session.close()
