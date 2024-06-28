f6bbee2f0fbd3fdf27a1086002c832cc2ebcb37e
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-29 01:24:13 +0300

    cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,11 +2,8 @@
 # model_city.py
 
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from model_state import Base
-from sqlalchemy.ext.declarative import declarative_base
-
-
-Base = declarative_base()
 
 
 class City(Base):
@@ -15,3 +12,5 @@ class City(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    state = relationship("State", back_populates="cities")
