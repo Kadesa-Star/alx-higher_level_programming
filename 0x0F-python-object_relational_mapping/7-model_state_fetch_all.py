92614c4ec95820a36584ca99e3223e19be4ca629
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-28 22:21:44 +0300

    all states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,11 +11,12 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     # Database connection
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
 
     # Create a session
     session = Session()
