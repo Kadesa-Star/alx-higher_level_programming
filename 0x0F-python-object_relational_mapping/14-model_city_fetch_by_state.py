999ba7e495c9e4707e795a8d85e0ae45eff3700d
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-29 01:20:25 +0300

    cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # 14-model_city_fetch_by_state.py
 
 import sys
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,12 @@
+#!/usr/bin/python3
 # model_city.py
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 
 class City(Base):
