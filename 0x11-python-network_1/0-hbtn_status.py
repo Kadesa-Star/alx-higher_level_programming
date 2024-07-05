557329fa1396b0a29e01b597f52fda24555aa75e
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-07-05 23:14:45 +0300

    whats my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status using urllib.
+Displays various details of the response body with proper formatting.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    
+    with urllib.request.urlopen(url) as response:
+        raw_content = response.read()
+        content = raw_content.decode('utf-8')
+        
+        print("Body response:")
+        print(f"\t- type: {type(raw_content)}")
+        print(f"\t- content: {raw_content}")
+        print(f"\t- utf8 content: {content}")
