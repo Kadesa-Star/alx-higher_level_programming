e637b53969c1b439226baf9dde37222876c26cb3
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-07-06 00:18:13 +0300

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """
-Uses Basic Authentication with a GitHub personal access token to fetch user ID.
+Uses Basic Authentication with GitHub username and pat to fetch user ID.
 """
-import requests
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 3:
         print("Usage: ./github_id.py <username> <token>")
         sys.exit(1)
+
     username = sys.argv[1]
     token = sys.argv[2]
+
+    auth = HTTPBasicAuth(username, token)
     url = 'https://api.github.com/user'
+
     try:
-        response = requests.get(url, auth=(username, token))
-        if response.status_code == 200:
-            user_data = response.json()
-            print(user_data['id'])
-        else:
-            print(f"Error: {response.status_code}")
+        response = requests.get(url, auth=auth)
+        print(response.json().get('id'))
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
