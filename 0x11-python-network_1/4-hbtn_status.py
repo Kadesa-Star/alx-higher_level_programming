c9160ac99a576884691d0f3009fa16a3788b9325
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-07-05 23:45:02 +0300

    whats my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,5 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
     print("Body response:")
-    print(f"\t- type: {type(response.text)}")
-    print(f"\t- content: {response.text}")
-    print(f"\t- utf8 content: {response.text}")
+    print(f"    - type: {type(response.text)}")
+    print(f"    - content: {response.text}")
