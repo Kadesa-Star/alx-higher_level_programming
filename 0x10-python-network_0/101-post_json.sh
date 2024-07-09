7c58ba4b4e95ced87db5b142efed2676e0354e0f
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-07-09 19:02:01 +0300

    curl a json file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a URL passed as the first argument,
-# with the contents of a file passed as the second argument
-
-# Check if the correct number of arguments are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <URL> <filename>"
-    exit 1
-fi
-
-# Assign arguments to variables for readability
-URL=$1
-FILE=$2
-
-# Send a POST request with the contents of the file
-curl -s -X POST -H "Content-Type: application/json" -d @"$FILE" "$URL"
+# Script to send a JSON POST request with file contents
+curl -s -X POST -H "Content-Type: application/json" -d "@$2" "$1"
