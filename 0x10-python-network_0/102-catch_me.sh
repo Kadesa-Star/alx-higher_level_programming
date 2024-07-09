33666fa724559300df3f92a5e37605ddf9c07bf4
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-07-09 19:12:18 +0300

    catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script to make a request to cause the server to respond with "You got me!"
-
-curl -sX PUT -d "user_id=98" 0.0.0.0:5000/catch_me > /dev/null 2>&1
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
