89893247b4d97b147ec7f4dfdc2f256605e8a205
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-07 22:01:14 +0300

    no jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+// Wait until the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the <header> element
+  const header = document.querySelector('header');
+
+  // Update the text color of the <header> element to red
+  header.style.color = '#FF0000';
+});
