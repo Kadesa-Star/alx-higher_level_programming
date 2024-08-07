c529849f81d8c6af49782f5390364c894c5f1c10
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-07 20:43:38 +0300

    with JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Ensure the DOM is fully loaded before executing the script
+$(document).ready(function () {
+  // Select the <header> element and change its text color
+  $('header').css('color', '#FF0000');
+});
