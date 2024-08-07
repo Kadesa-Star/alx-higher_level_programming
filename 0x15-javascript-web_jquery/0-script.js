61d3bf07363e477004eb7dfb76b01cbef74868c3
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-08-07 20:28:40 +0300

    No JQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Select the <header> element usind document.querySelector
+const header = document.querySelector('header');
+
+// change the text color of the <header> element to red
+header.style.color = '#FF0000';
